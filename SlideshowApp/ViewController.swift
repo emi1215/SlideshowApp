8594b1bb90f636a81a85f53cee76df87461fc4a5
emi <bluming_blue@icloud.com>
2022-03-02 14:47:39 +0900

    エラーの理由がわかりません。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var go: UIButton!
+    @IBOutlet weak var back: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var nowIndex:Int = 0
+    
+    var timer: Timer!
+    
+    var imageArray:[UIImage] = [
+        UIImage(named: "00")!,
+        UIImage(named: "01")!,
+        UIImage(named: "02")!,
+        UIImage(named: "07")!,
+        UIImage(named: "19")!,
+        UIImage(named: "28")!
+        ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func slideShowButton(_ sender: Any) {
+        if (timer == nil) {
+            
+                   timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            
 
-
+               } else {
+                   
+                   timer.invalidate()
+                   
+                   timer = nil
+                   
+               }
+    }
+    
+    @objc func changeImage() {
+        
+            nowIndex += 1
+        
+            if (nowIndex == imageArray.count) {
+                
+                nowIndex = 0
+            }
+        
+            imageView.image = imageArray[nowIndex]
+        }
 }
 
